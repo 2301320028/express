fef716420b46520482f9d3d7cf6f44b99537bcf0
2301320028 <2301320028ug@cyber-u.ac.jp>
2025-01-31 22:18:28 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
   res.render('index', { title: 'Express2' });
-=======
-
->>>>>>> ac3039621d03e23d55a38156831661fe42cf9788
 });
 
 module.exports = router;
